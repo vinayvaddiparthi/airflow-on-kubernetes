17173ee3464e59b843aaf5f6adc84766d263b008
Xiaotu Zhang <xzhang@thinkingcapital.ca>
2019-10-03 16:59:20 -0400

    Add sas to redshift dag and a few common operators

---
+++ dags/common/operator_query.py
@@ -0,0 +1,27 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+
+
+class QueryOperator(BaseOperator):
+    ui_color = "#ebf5ff"
+    ui_fgcolor = "#5e6266"
+
+    def __init__(
+            self,
+            task_id: str,
+            sql: list,
+            postgres_conn_id: str,
+            callable=None,
+            *args,
+            **kwargs
+    ):
+        super().__init__(task_id=task_id, *args, **kwargs)
+        self.sql = sql
+        self.postgres_hook = PostgresHook(postgres_conn_id=postgres_conn_id)
+        self.callable = callable
+
+    def execute(self, context):
+        result = self.postgres_hook.get_first(self.sql)
+        error = callable(result)
+        if error:
+            raise error
