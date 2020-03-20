57c8f152b85e6d975f433e0eb9143e04ab37ff27
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2020-03-20 14:21:55 -0400

    Add new heroku-based platform import DAG
    This initial commit only contains the CORE_PRODUCTION schema for testing
    purposes. Once we verify that everything works as expected, the other databases
    and staging environment dumps will be added.

--- dags/import_platform_dag.py
+++ dags/import_platform_dag.py
@@ -1,169 +1,56 @@
-from typing import Dict
+import os
 
 import pendulum
-from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
+from airflow.hooks.http_hook import HttpHook
 from airflow.operators.python_operator import PythonOperator
-from sqlalchemy import text, and_, create_engine, cast, VARCHAR, column, JSON, func
-from sqlalchemy.sql import Select, label
 from airflow import DAG
+import subprocess
 
-PRESTO_ADDR = "presto://presto-production-internal.presto.svc:8080"
 
+def run_heroku_command(app: str, snowflake_connection, snowflake_schema):
+    os.environ["HEROKU_API_KEY"] = HttpHook("heroku_production").get_conn().auth[1]
 
-def format_load_as_json_query(catalog: str, schema: str, table: str) -> Select:
-    """
-    Takes a catalog, schema and table as input and returns a Select object
-    that represents a query to load the data and transform it in JSON.
-
-    :param catalog:
-    :param schema:
-    :param table:
-    :return:
-    """
-    from_obj = text(f'"{catalog}"."{schema}"."{table}"')
-
-    column_names_selectable = Select(
-        columns=[text("column_name")],
-        from_obj=text(f'"{catalog}"."information_schema"."columns"'),
-        whereclause=and_(
-            text(f"\"table_catalog\" = '{catalog}'"),
-            text(f"\"table_schema\" = '{schema}'"),
-            text(f"\"table_name\" = '{table}'"),
-        ),
+    subprocess.run(["ssh-keygen", "-t", "rsa", "-N", "", "-f", "id_rsa"])
+    subprocess.run(["ssh-add", "id_rsa"])
+    subprocess.run(["heroku", "keys:add", "id_rsa.pub"])
+    subprocess.run(
+        [
+            "heroku",
+            "run",
+            "-a",
+            app,
+            "-e",
+            f"SNOWFLAKE_PASSWORD={snowflake_connection.password}",
+            "python",
+            "extract.py",
+            "--snowflake-account",
+            "thinkingcapital.ca-central-1.aws",
+            "--snowflake-username",
+            snowflake_connection.login,
+            "--snowflake-password",
+            "$SNOWFLAKE_PASSWORD",
+            "--snowflake-database",
+            "ZETATANGO",
+            "--snowflake-schema",
+            snowflake_schema,
+        ]
     )
-
-    with create_engine(PRESTO_ADDR).begin() as tx:
-        resultset = [x[0] for x in tx.execute(column_names_selectable).fetchall()]
-
-    column_names = [f"'{column(x)}'" for x in resultset]
-    casts = [f"{cast(column(x), VARCHAR)}" for x in resultset]
-    names_array = text(f"ARRAY[{', '.join(column_names)}]")
-    expr_array = text(f"ARRAY[{', '.join(casts)}]")
-
-    json_fn = func.json_format(cast(func.map(names_array, expr_array), JSON))
-
-    return Select(columns=[json_fn], from_obj=from_obj)
+    subprocess.run(["heroku", "keys:clear"])
 
 
-def load(src: Dict[str, str], dst: Dict[str, str]) -> None:
-    """
-    Extracts data from source table src and loads it into table dst in JSON format.
-
-    :param src:
-    :param dst:
-    :return:
-    """
-
-    to_obj = text(f'"{dst["catalog"]}"."{dst["schema"]}"."{dst["table"]}"')
-
-    selectable = format_load_as_json_query(src["catalog"], src["schema"], src["table"])
-
-    with create_engine(PRESTO_ADDR).begin() as tx:
-        tx.execute(f"CREATE TABLE IF NOT EXISTS {to_obj} AS {selectable}").fetchall()
-
-
-def snowflake_swap(src: Dict[str, str], dst: Dict[str, str]) -> None:
-    """
-    Performs a table swap on the destination table in Snowflake in a transaction.
-
-    :param src:
-    :param dst:
-    :return:
-    """
-    from_obj = text(
-        f'"{src["catalog"].upper()}"."{src["schema"].upper()}"."{src["table"].upper()}"'
-    )
-    to_obj = text(
-        f'"{dst["catalog"].upper()}"."{dst["schema"].upper()}"."{dst["table"].upper()}"'
-    )
-
-    with SnowflakeHook("snowflake_default").get_sqlalchemy_engine().begin() as tx:
-        selectable = Select(
-            columns=[label("data", func.parse_json(column("json_format_1")))],
-            from_obj=from_obj,
-        )
-
-        tx.execute(f"CREATE OR REPLACE TABLE {to_obj} AS {selectable}").fetchall()
-        tx.execute(f"DROP TABLE {from_obj}").fetchall()
-
-
-def _create_dag(
-    dag_id: str,
-    start_date: pendulum.datetime,
-    presto_input_catalog: str,
-    presto_output_catalog: str,
-    snow_dbname: str,
-    schema: str,
-) -> DAG:
-    SUFFIX = "SWAP"
-
-    stmt = Select(
-        columns=[column("table_catalog"), column("table_schema"), column("table_name")],
-        from_obj=text(f'"{presto_input_catalog}"."information_schema"."tables"'),
-        whereclause=text(f"table_schema = '{schema}'"),
-    )
-
-    with create_engine(PRESTO_ADDR).begin() as tx:
-        resultset = tx.execute(stmt).fetchall()
-
-    with DAG(dag_id=dag_id, start_date=start_date, schedule_interval="@daily",) as dag:
-        for catalog, schema, table in resultset:
-            dag << PythonOperator(
-                task_id=f"load__{catalog}__{schema}__{table}",
-                python_callable=load,
-                op_kwargs={
-                    "src": {"catalog": catalog, "schema": schema, "table": table,},
-                    "dst": {
-                        "catalog": presto_output_catalog,
-                        "schema": schema,
-                        "table": f"{table}__{SUFFIX}",
-                    },
-                },
-            ) >> PythonOperator(
-                task_id=f"snowflake_swap__{catalog}__{schema}__{table}",
-                python_callable=snowflake_swap,
-                op_kwargs={
-                    "src": {
-                        "catalog": snow_dbname,
-                        "schema": schema,
-                        "table": f"{table}__{SUFFIX}",
-                    },
-                    "dst": {"catalog": snow_dbname, "schema": schema, "table": table,},
-                },
-            )
-
-    return dag
-
-
-globals()["import__zt_production_core"] = _create_dag(
-    "import__zt_production_core",
+with DAG(
+    dag_id="platform_import_production",
     start_date=pendulum.datetime(
-        2020, 2, 27, tzinfo=pendulum.timezone("America/Toronto"),
+        2020, 3, 20, tzinfo=pendulum.timezone("America/Toronto")
     ),
-    presto_input_catalog="glue",
-    presto_output_catalog="sf_zt_production",
-    schema="zt_production_core",
-    snow_dbname="ZT_PRODUCTION",
-)
-
-globals()["import__zt_production_idp"] = _create_dag(
-    "import__zt_production_idp",
-    start_date=pendulum.datetime(
-        2020, 2, 27, tzinfo=pendulum.timezone("America/Toronto"),
-    ),
-    presto_input_catalog="glue",
-    presto_output_catalog="sf_zt_production",
-    schema="zt_production_idp",
-    snow_dbname="ZT_PRODUCTION",
-)
-
-globals()["import__zt_production_kyc"] = _create_dag(
-    "import__zt_production_kyc",
-    start_date=pendulum.datetime(
-        2020, 2, 27, tzinfo=pendulum.timezone("America/Toronto"),
-    ),
-    presto_input_catalog="glue",
-    presto_output_catalog="sf_zt_production",
-    schema="zt_production_kyc",
-    snow_dbname="ZT_PRODUCTION",
-)
+    schedule_interval="@hourly",
+) as dag:
+    dag << PythonOperator(
+        task_id="import_core_production",
+        python_callable=run_heroku_command,
+        op_kwargs={
+            "app": "zt-production-elt-core",
+            "snowflake_connection": "snowflake_zetatango_production",
+            "snowflake_schema": "CORE_PRODUCTION",
+        },
+    )
