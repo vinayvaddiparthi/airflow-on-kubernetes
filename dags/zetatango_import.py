71cd787962a21efb66f8cd4ae1df0172de45da0a
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2020-04-02 13:14:12 -0400

    Add decryption task to ZT import DAG

--- dags/zetatango_import.py
+++ dags/zetatango_import.py
@@ -2,13 +2,27 @@ import logging
 import os
 import tempfile
 from pathlib import Path
+from typing import List
 
+import attr
+import pandas as pd
 import pendulum
 from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
 from airflow.hooks.http_hook import HttpHook
 from airflow.operators.python_operator import PythonOperator
 from airflow import DAG
 import subprocess  # nosec
+
+from sqlalchemy import text, func
+from sqlalchemy.sql import Select
+
+
+@attr.s
+class ColumnSpec:
+    schema: str = attr.ib()
+    table: str = attr.ib()
+    columns: List[str] = attr.ib()
+    catalog: str = attr.ib(default=None)
 
 
 def run_heroku_command(app: str, snowflake_connection: str, snowflake_schema: str):
@@ -52,6 +66,56 @@ def run_heroku_command(app: str, snowflake_connection: str, snowflake_schema: st
         completed_process.check_returncode()
 
 
+def decrypt_pii_columns(snowflake_connection: str, column_specs: List[ColumnSpec]):
+    from pyporky.symmetric import SymmetricPorky
+    from json import loads as json_loads
+    from base64 import b64decode
+
+    def __decrypt(row):
+        list_ = [row[0]]
+        for field in row[1:]:
+            crypto_material = json_loads(field)
+            list_.append(
+                SymmetricPorky(aws_region="ca-central-1").decrypt(
+                    enciphered_dek=b64decode(crypto_material["key"]),
+                    enciphered_data=b64decode(crypto_material["data"]),
+                    nonce=b64decode(crypto_material["nonce"]),
+                )
+            )
+
+        return list_
+
+    for cs in column_specs:
+        stmt = Select(
+            columns=[text(f"{cs.table}.$1:id AS id")]
+            + [
+                func.base64_decode_string(text(f"{cs.table}.$1:encrypted_{col}")).label(
+                    col
+                )
+                for col in cs.columns
+            ],
+            from_obj=text(f"{cs.schema}.{cs.table}"),
+        ).limit(10)
+
+        engine = SnowflakeHook(snowflake_connection).get_sqlalchemy_engine()
+        with engine.begin() as tx, tempfile.TemporaryDirectory() as output_directory:
+            path = Path(output_directory) / f"{cs.table}.parquet"
+
+            df = pd.read_sql(stmt, con=tx).apply(
+                axis=1, func=__decrypt, result_type="broadcast"
+            )
+            df.to_parquet(path, engine="fastparquet", compression="gzip")
+
+            return [
+                tx.execute(stmt).fetchall()
+                for stmt in [
+                    f"CREATE OR REPLACE STAGE {cs.schema}.{cs.table}__PII FILE_FORMAT=(TYPE=PARQUET)",
+                    f"PUT file://{path} @{cs.schema}.{cs.table}",
+                    f"CREATE OR REPLACE TABLE {cs.schema}.{cs.table}__PII AS SELECT * FROM @{cs.schema}.{cs.table}__PII",
+                ]
+            ]
+
+
 with DAG(
     dag_id="zetatango_import",
     start_date=pendulum.datetime(
@@ -59,40 +123,82 @@ with DAG(
     ),
     schedule_interval="0 0,8-20 * * 1-5",
 ) as dag:
-    for kwargs in [
-        {
+    dag << PythonOperator(
+        task_id="zt-production-elt-core__import",
+        python_callable=run_heroku_command,
+        op_kwargs={
             "app": "zt-production-elt-core",
             "snowflake_connection": "snowflake_zetatango_production",
             "snowflake_schema": "CORE_PRODUCTION",
         },
-        {
-            "app": "zt-production-elt-kyc",
-            "snowflake_connection": "snowflake_zetatango_production",
-            "snowflake_schema": "KYC_PRODUCTION",
-        },
-        {
+    )
+
+    dag << PythonOperator(
+        task_id="zt-production-elt-idp__import",
+        python_callable=run_heroku_command,
+        op_kwargs={
             "app": "zt-production-elt-idp",
             "snowflake_connection": "snowflake_zetatango_production",
             "snowflake_schema": "IDP_PRODUCTION",
         },
-        {
+    )
+
+    dag << PythonOperator(
+        task_id="zt-production-elt-kyc__import",
+        python_callable=run_heroku_command,
+        op_kwargs={
+            "app": "zt-production-elt-kyc",
+            "snowflake_connection": "snowflake_zetatango_production",
+            "snowflake_schema": "KYC_PRODUCTION",
+        },
+    )
+
+    dag << PythonOperator(
+        task_id="zt-staging-elt-core__import",
+        python_callable=run_heroku_command,
+        op_kwargs={
             "app": "zt-staging-elt-core",
             "snowflake_connection": "snowflake_zetatango_staging",
             "snowflake_schema": "CORE_STAGING",
         },
-        {
-            "app": "zt-staging-elt-kyc",
+    ) >> PythonOperator(
+        task_id="zt-staging-elt-core__pii_decryption",
+        python_callable=decrypt_pii_columns,
+        op_kwargs={
             "snowflake_connection": "snowflake_zetatango_staging",
-            "snowflake_schema": "KYC_STAGING",
+            "column_specs": [
+                ColumnSpec(
+                    schema="CORE_STAGING",
+                    table="MERCHANT_ATTRIBUTES",
+                    columns=["value"],
+                )
+            ],
         },
-        {
+        executor_config={
+            "KubernetesExecutor": {
+                "annotations": {
+                    "iam.amazonaws.com/role": "arn:aws:iam::810110616880:role/KubernetesAirflowNonProdZetatangoPiiRole"
+                }
+            }
+        },
+    )
+
+    dag << PythonOperator(
+        task_id="zt-staging-elt-idp__import",
+        python_callable=run_heroku_command,
+        op_kwargs={
             "app": "zt-staging-elt-idp",
             "snowflake_connection": "snowflake_zetatango_staging",
             "snowflake_schema": "IDP_STAGING",
         },
-    ]:
-        dag << PythonOperator(
-            task_id=f'import_{kwargs["app"]}',
-            python_callable=run_heroku_command,
-            op_kwargs=kwargs,
-        )
+    )
+
+    dag << PythonOperator(
+        task_id="zt-production-elt-kyc__import",
+        python_callable=run_heroku_command,
+        op_kwargs={
+            "app": "zt-staging-elt-kyc",
+            "snowflake_connection": "snowflake_zetatango_staging",
+            "snowflake_schema": "KYC_STAGING",
+        },
+    )
