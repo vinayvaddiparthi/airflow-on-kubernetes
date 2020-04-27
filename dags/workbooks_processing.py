a0d7f7d5b83320db0960656c427b626f0d26cced
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2020-04-26 20:44:34 -0400

    Use CSV instead of parquet to dump workbooks content

--- dags/workbooks_processing.py
+++ dags/workbooks_processing.py
@@ -80,13 +80,13 @@ def import_workbooks(
     print(f"✔️ Done computing 18 characters SFDC object IDs")
 
     engine_ = SnowflakeHook(snowflake_conn).get_sqlalchemy_engine()
-    with engine_.begin() as tx, tempfile.NamedTemporaryFile() as path:
-        df.to_parquet(f"{path}", engine="fastparquet", compression="gzip")
+    with engine_.begin() as tx, tempfile.NamedTemporaryFile() as file:
+        df.to_csv(path_or_buf=file, compression="gzip")
         print("Dataframe converted to parquet")
 
         stmts = [
-            f"CREATE OR REPLACE TEMPORARY STAGE {destination_schema}.{stage_guid} FILE_FORMAT=(TYPE=PARQUET)",  # nosec
-            f"PUT file://{path} @{destination_schema}.{stage_guid}",  # nosec
+            f"CREATE OR REPLACE TEMPORARY STAGE {destination_schema}.{stage_guid} FILE_FORMAT=(TYPE=CSV)",  # nosec
+            f"PUT file://{file.name} @{destination_schema}.{stage_guid}",  # nosec
             f"CREATE OR REPLACE TRANSIENT TABLE {destination_schema}.{destination_table} AS SELECT * FROM @{destination_schema}.{stage_guid}",  # nosec
         ]
 
