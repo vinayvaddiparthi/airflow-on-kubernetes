94c9d7ed1845859924bd00c3f288f67fcabbde41
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2020-05-03 12:59:29 -0400

    Use static IAM credentials for workbooks processing

--- dags/workbooks_processing.py
+++ dags/workbooks_processing.py
@@ -116,9 +116,10 @@ with DAG(
         "apt-get install -y cifs-utils wget && "
         "wget https://dl.min.io/client/mc/release/linux-amd64/mc && "
         "chmod +x mc && mv mc /usr/local/bin && "
+        "mkdir ~/.mc &&"
+        "cp /secrets/mc-config ~/.mc/config.json && "
         "mkdir /workbooks && "
         'mount -t cifs "//10.10.1.110/Working Files" /workbooks -o credentials=/secrets/auth && '
-        "mc config host add s3 https://s3.amazonaws.com && "
         "mc mirror --overwrite /workbooks s3://tc-workbooks && "
         "umount /workbooks"
     ]
@@ -133,10 +134,6 @@ with DAG(
         name="grab_workbooks",
         is_delete_operator_pod=True,
         security_context={"privileged": True},
-        annotations={
-            "iam.amazonaws.com/role": "arn:aws:iam::810110616880:role/"
-            "KubernetesAirflowProductionWorkbooksRole"
-        },
         executor_config={
             "KubernetesExecutor": {"service_account_name": "airflow-scheduler"}
         },
