bb4898363bb589ed589281c72346a6512247140b
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2020-07-15 01:24:13 -0400

    Remove netsuite_extras
    Now unused

--- dags/netsuite_extras/queries/log_journal_entry_status.sql
+++
@@ -1,8 +0,0 @@
-insert into erp.{{env}}.journal_entry_status
-values ('{{je_internal_id}}',
-        '{{error_msg}}',
-        '{{created_date}}',
-        '{{transaction_date}}',
-        '{{execution_time}}',
-        '{{uploaded}}',
-        '{{subsidiary_id}}');
