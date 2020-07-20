a9d03bc39d22a21f41f06c57073ad528a6590c45
michellefoy <michelle.foy@zetatango.com>
2020-07-20 15:54:40 -0400

    Format properly

--- dags/dbt_runner.py
+++ dags/dbt_runner.py
@@ -6,20 +6,18 @@ from dbt_extras.dbt_action import DbtAction
 from airflow.operators.sensors import ExternalTaskSensor
 
 with DAG(
-        "dbt_runner",
-        max_active_runs=1,
-        schedule_interval=None,
-        start_date=pendulum.datetime(
-            2020, 4, 21, tzinfo=pendulum.timezone("America/Toronto")
-        ),
-        check_data_source=
-
+    "dbt_runner",
+    max_active_runs=1,
+    schedule_interval=None,
+    start_date=pendulum.datetime(
+        2020, 4, 21, tzinfo=pendulum.timezone("America/Toronto")
+    ),
 ) as dag:
     dag << ExternalTaskSensor(
-        task_id='dbt_check',
-        external_dag_id='zetatango_import',
+        task_id="dbt_check",
+        external_dag_id="zetatango_import",
         execution_delta=timedelta(hours=1),
-        timeout=300
+        timeout=300,
     ) << DbtOperator(
         task_id="dbt_run",
         pool="snowflake_pool",
