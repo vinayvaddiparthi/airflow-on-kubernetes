80f1e3b7cf19e16dd182b4a6e499ca5df9edf2e8
michellefoy <michelle.foy@zetatango.com>
2020-07-20 15:52:13 -0400

    Use ExternalTaskSensor

--- dags/dbt_runner.py
+++ dags/dbt_runner.py
@@ -3,20 +3,28 @@ import datetime
 import pendulum
 from dbt_extras.dbt_operator import DbtOperator
 from dbt_extras.dbt_action import DbtAction
-
+from airflow.operators.sensors import ExternalTaskSensor
 
 with DAG(
-    "dbt_runner",
-    max_active_runs=1,
-    schedule_interval=None,
-    start_date=pendulum.datetime(
-        2020, 4, 21, tzinfo=pendulum.timezone("America/Toronto")
-    ),
+        "dbt_runner",
+        max_active_runs=1,
+        schedule_interval=None,
+        start_date=pendulum.datetime(
+            2020, 4, 21, tzinfo=pendulum.timezone("America/Toronto")
+        ),
+        check_data_source=
+
 ) as dag:
-    dag << DbtOperator(
+    dag << ExternalTaskSensor(
+        task_id='dbt_check',
+        external_dag_id='zetatango_import',
+        execution_delta=timedelta(hours=1),
+        timeout=300
+    ) << DbtOperator(
         task_id="dbt_run",
         pool="snowflake_pool",
         execution_timeout=datetime.timedelta(hours=1),
+        action=DbtAction.run,
     ) >> DbtOperator(
         task_id="dbt_snapshot",
         pool="snowflake_pool",
