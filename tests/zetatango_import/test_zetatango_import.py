ec74d5ce10bb26f1f80af01a3ba854a084181925
michellefoy <michelle.foy@zetatango.com>
2020-07-29 17:08:40 -0400

    Add basic zetatango import test

---
+++ tests/zetatango_import/test_zetatango_import.py
@@ -0,0 +1,14 @@
+from airflow.models import DagBag
+import unittest
+
+
+class TestZetatangoImportDag(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dagbag = DagBag()
+
+    def test_dag_loaded(self):
+        dag = self.dagbag.get_dag(dag_id='zetatango_import')
+        self.assertDictEqual(self.dagbag.import_errors, {})
+        self.assertIsNotNone(dag)
+        self.assertEqual(len(dag.tasks), 1)
