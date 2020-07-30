95b5db8f13fe3a9611e3540bca3146697cadf9ca
Michelle Foy <michelle.foy@arioplatform.com>
2020-07-30 19:54:50 +0000

    Add dbt tasks directly in zetatango_import DAG

--- dags/dbt_runner.py
+++ dags/dbt_runner.py
@@ -6,15 +6,11 @@ from dbt_extras.dbt_action import DbtAction
 
 
 dbt_run = DbtOperator(
-    task_id="dbt_run",
-    pool="snowflake_pool",
-    execution_timeout=timedelta(hours=1),
-    action=DbtAction.run,
+    task_id="dbt_run", execution_timeout=timedelta(hours=1), action=DbtAction.run,
 )
 
 dbt_snapshot = DbtOperator(
     task_id="dbt_snapshot",
-    pool="snowflake_pool",
     execution_timeout=timedelta(hours=1),
     action=DbtAction.snapshot,
 )
