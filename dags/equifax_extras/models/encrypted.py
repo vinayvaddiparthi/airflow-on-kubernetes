591b426b38c2cd65752dd12c3e48b0ee4e43ba6a
Brandon Vrooman <brandon.vrooman@arioplatform.com>
2020-08-12 14:35:52 -0400

    Refactor

--- dags/equifax_extras/models/encrypted.py
+++ dags/equifax_extras/models/encrypted.py
@@ -1,13 +1,9 @@
 import json
 from base64 import b64decode
 from pyporky.symmetric import SymmetricPorky
-import os
+from airflow.models import Variable
 
-os.environ["AWS_ACCESS_KEY_ID"] = "AKIAI6CT7DNHO7PW6IIA"
-os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
-print(os.environ["AWS_ACCESS_KEY_ID"])
-print(os.environ["AWS_SECRET_ACCESS_KEY"])
-porky = SymmetricPorky(aws_region="ca-central-1")
+porky = SymmetricPorky(aws_region=Variable.get("aws_kms_region", default_var='ca-central-1'))
 
 
 def encrypted(func):
