34ddb8a91c52eee3d2100104159ee9141fa6cde0
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2020-10-08 11:35:20 -0400

    Add failure callback to zetatango import DAG

--- dags/utils/failure_callbacks.py
+++ dags/utils/failure_callbacks.py
@@ -8,7 +8,18 @@ def slack_ti(conn_id: str) -> Callable:
         return SlackWebhookOperator(
             task_id="slack_fail",
             http_conn_id=conn_id,
-            message=f"<!here> {context['ti']}",
+            message=f"{context['ti']}",
+        ).execute(context=context)
+
+    return func
+
+
+def slack_dag(conn_id: str) -> Callable:
+    def func(context: Dict) -> str:
+        return SlackWebhookOperator(
+            task_id="slack_fail",
+            http_conn_id=conn_id,
+            message=f'DagRun failed: {context["dag_run"]}',
         ).execute(context=context)
 
     return func
