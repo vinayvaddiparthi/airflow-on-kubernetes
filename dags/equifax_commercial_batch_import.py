3ddddb602a549c543cd42ca5940f516d032fd36e
Xiaotu Zhang <xzhang@thinkingcapital.ca>
2020-11-13 10:03:17 -0500

    Temporarily remove the sftp download part of code

--- dags/equifax_commercial_batch_import.py
+++ dags/equifax_commercial_batch_import.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 from typing import Any
-from equifax_extras.equifax_sftp_util import download
 from equifax_extras.commercial_batch_data_type import dt_comm, dt_tcap
 import boto3
 import pandas as pd
@@ -9,7 +8,7 @@ from airflow import DAG
 from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
 from airflow.contrib.hooks.ssh_hook import SSHHook
-from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
+from airflow.operators.python_operator import PythonOperator
 
 bucket = "tc-datalake"
 commercial_prefix = "equifax_offline_batch/commercial/output/"
@@ -27,49 +26,6 @@ def get_month_tag(ds: str) -> str:
     else:
         scheduled = then.add(months=-2)
     return f"{scheduled.year}{str(scheduled.month).zfill(2)}"
-
-
-def check_bucket(ds: str, **context: Any) -> str:
-    """
-    if month folder exist in output folder, means we can skip
-    """
-    client = boto3.client(
-        "s3",
-        aws_access_key_id=aws_credentials.access_key,
-        aws_secret_access_key=aws_credentials.secret_key,
-    )
-    month_folder = f"{commercial_prefix}{get_month_tag(ds)}/"
-    objects = client.list_objects(Bucket=bucket, Prefix=month_folder, Delimiter="/")
-    for prefix in objects["CommonPrefixes"]:
-        file = prefix["Prefix"].split("/")[-1]
-        print(file)
-        return "end"
-    return "commercial_batch_import"
-
-
-def download_from_sftp(ds: str, **context: Any) -> str:
-    # This should contain
-    # 1. download from sftp
-    # 2. upload file to s3
-    # If download got null, skip to the end
-
-    #  All files in sftp outbox folder will be downloaded to current directory
-    files = download(ssh_hook.remote_host, ssh_hook.username, ssh_hook.password)
-    if files:
-        for file in files:
-            with open(file, "rb"):
-                client = boto3.client(
-                    "s3",
-                    aws_access_key_id=aws_credentials.access_key,
-                    aws_secret_access_key=aws_credentials.secret_key,
-                )
-                print(f"Upload downloaded file to s3: {file}")
-                path = commercial_prefix + get_month_tag(ds)
-                print(f"Destination: {path}")
-                # upload converted csv to the same folder of output file
-                client.upload_fileobj(file, bucket, path)
-        return "commercial_batch_import"
-    return "end"
 
 
 def commercial_batch_import(ds: str, **context: Any) -> None:
@@ -133,10 +89,6 @@ def commercial_batch_import(ds: str, **context: Any) -> None:
                 conn.commit()
 
 
-def end() -> None:
-    print("end")
-
-
 with DAG(
     "equifax_commercial_output_dag",
     max_active_runs=1,
@@ -146,30 +98,9 @@ with DAG(
     ),
     default_args={"retries": 3, "retry_delay": timedelta(minutes=30)},
 ) as dag:
-    op_commercial_batch_import = PythonOperator(
+    dag << PythonOperator(
         task_id="commercial_batch_import",
         python_callable=commercial_batch_import,
         provide_context=True,
         dag=dag,
     )
-    op_check_bucket = BranchPythonOperator(
-        task_id="check_bucket",
-        python_callable=check_bucket,
-        provide_context=True,
-        dag=dag,
-    )
-    op_download_from_sftp = BranchPythonOperator(
-        task_id="download_from_sftp",
-        python_callable=download_from_sftp,
-        provide_context=True,
-        dag=dag,
-    )
-    op_end = PythonOperator(
-        task_id="end",
-        python_callable=end,
-        provide_context=True,
-        dag=dag,
-    )
-
-    op_check_bucket >> [op_download_from_sftp, op_end]
-    op_download_from_sftp >> [op_commercial_batch_import, op_end]
