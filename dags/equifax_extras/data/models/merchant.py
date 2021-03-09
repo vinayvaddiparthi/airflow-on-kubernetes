24e5a3932ad9ed24a6ddd2adc4aa82328d9ce902
Brandon Vrooman <brandon.vrooman@arioplatform.com>
2021-03-08 22:32:25 -0500

    Add file number to commercial batch request

--- dags/equifax_extras/data/models/merchant.py
+++ dags/equifax_extras/data/models/merchant.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Table, Column, Integer
 
+from equifax_extras.data.decorators import to_string, encrypted, marshalled
 from equifax_extras.data.models.base import Base, metadata
 
 
@@ -9,6 +10,7 @@ merchant_table = Table(
     Column("id", Integer, primary_key=True),
     Column("guid"),
     Column("name"),
+    Column("encrypted_file_number")
 )
 
 
@@ -20,3 +22,10 @@ class Merchant(Base):
 
     def __repr__(self) -> str:
         return f"Merchant({self})"
+
+    @property  # type: ignore
+    @to_string
+    @marshalled
+    @encrypted
+    def file_number(self) -> str:
+        return self.encrypted_file_number
