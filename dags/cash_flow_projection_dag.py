279ce1660b3bbd38b89af346d0bc64d2333f65cf
Brent Carrara <brent.carrara@zetatango.com>
2021-03-15 06:07:44 -0400

    feat: Add task for multi-account projection.

--- dags/cash_flow_projection_dag.py
+++ dags/cash_flow_projection_dag.py
@@ -4,6 +4,7 @@ import pendulum
 import pandas as pd
 import attr
 import sys
+import sqlalchemy
 
 from airflow import DAG
 from airflow.models.dagrun import DagRun
@@ -30,7 +31,10 @@ from helpers.auto_arima_parameters import (
     CashFlowProjectionParameters,
 )
 
-from sqlalchemy.sql import select, func, text, literal_column
+from sqlalchemy.sql import select, func, text, literal_column, literal
+from sqlalchemy.engine import RowProxy
+from sqlalchemy.sql.selectable import Select
+from snowflake.sqlalchemy import VARIANT
 from sqlalchemy import Table, MetaData
 
 from helpers.rabbit_mq_helper import notify_subscribers
@@ -135,6 +139,64 @@ def calculate_projection(
     return prediction_df
 
 
+def skip_multi_account_projection(
+    merchant_guid: str,
+    account_guids: List[str],
+    snowflake_zetatango_connection: str,
+    zetatango_schema: str,
+    parameters: Dict[str, Dict[str, Any]],
+) -> bool:
+    metadata = MetaData()
+    zetatango_engine = SnowflakeHook(
+        snowflake_zetatango_connection
+    ).get_sqlalchemy_engine()
+
+    multi_account_cash_flow_projections = Table(
+        "multi_account_cash_flow_projections",
+        metadata,
+        autoload=True,
+        autoload_with=zetatango_engine,
+        schema=zetatango_schema,
+    )
+
+    select_query = select(
+        columns=[
+            multi_account_cash_flow_projections.c.merchant_guid,
+            multi_account_cash_flow_projections.c.parameters_hash,
+            text("1"),
+        ],
+        from_obj=multi_account_cash_flow_projections,
+    ).where(
+        multi_account_cash_flow_projections.c.merchant_guid == literal(merchant_guid)
+    )
+
+    existing_projections_df = pd.read_sql_query(
+        select_query,
+        zetatango_engine,
+        index_col=[
+            "merchant_guid",
+            "parameters_hash",
+        ],
+    )
+
+    digest = sha256(json.dumps(parameters).encode("utf-8")).hexdigest()
+
+    try:
+        existing_projections_df.loc[
+            (
+                merchant_guid,
+                sha256(json.dumps(parameters).encode("utf-8")).hexdigest(),
+            )
+        ]
+
+        logging.info(
+            f"⏩️️ Skipping generating projections for {merchant_guid} - {account_guids}"
+        )
+        return True
+    except KeyError:
+        return False
+
+
 def skip_projection(
     merchant_guid: str,
     account_guid: str,
@@ -237,6 +299,59 @@ def apply_post_projection_guardrail_closing_balance(
     )
 
     return not (min_balance <= closing_predicted_balance <= max_balance)
+
+
+def store_multi_account_projection(
+    merchant_guid: str,
+    account_guids: List[str],
+    cash_flow_df: pd.DataFrame,
+    parameters_to_hash: Dict[str, Dict[str, Any]],
+    details: Dict[str, Any],
+    snowflake_zetatango_connection: str,
+    zetatango_schema: str,
+) -> None:
+    metadata = MetaData()
+    zetatango_engine = SnowflakeHook(
+        snowflake_zetatango_connection
+    ).get_sqlalchemy_engine()
+
+    multi_account_cash_flow_projections = Table(
+        "multi_account_cash_flow_projections",
+        metadata,
+        autoload=True,
+        autoload_with=zetatango_engine,
+        schema=zetatango_schema,
+    )
+
+    with zetatango_engine.begin() as tx:
+        select_query = select(
+            columns=[
+                literal_column(f"'{merchant_guid}'").label("merchant_guid"),
+                func.array_construct(*account_guids).label("account_guids"),
+                literal_column(
+                    f"'{sha256(json.dumps(parameters_to_hash).encode('utf-8')).hexdigest()}'"
+                ).label("parameters_hash"),
+                func.parse_json(json.dumps(details)).label("projections"),
+                func.current_timestamp().label("generated_at"),
+            ]
+        )
+
+        insert_query = multi_account_cash_flow_projections.insert().from_select(
+            [
+                "merchant_guid",
+                "account_guids",
+                "parameters_hash",
+                "projections",
+                "generated_at",
+            ],
+            select_query,
+        )
+
+        tx.execute(insert_query)
+
+    logging.info(
+        f"✔️ Successfully stored projections for {merchant_guid} - {account_guids}"
+    )
 
 
 def store_projection(
@@ -409,6 +524,387 @@ def do_projection(
     )
 
 
+def last_transaction_date_query(
+    snowflake_analytics_connection: str,
+    analytics_schema: str,
+    merchant_guid: str,
+    account_guids: List[str],
+) -> Select:
+    metadata = MetaData()
+    production_engine = SnowflakeHook(
+        snowflake_analytics_connection
+    ).get_sqlalchemy_engine()
+
+    fct_daily_bank_account_balance = Table(
+        "fct_daily_bank_account_balance",
+        metadata,
+        autoload=True,
+        autoload_with=production_engine,
+        schema=analytics_schema,
+    )
+
+    last_transaction_by_account_query = (
+        select(
+            columns=[
+                fct_daily_bank_account_balance.c.merchant_guid,
+                fct_daily_bank_account_balance.c.account_guid,
+                func.max(fct_daily_bank_account_balance.c.date).label(
+                    "last_transaction"
+                ),
+            ],
+            from_obj=fct_daily_bank_account_balance,
+        )
+        .group_by(
+            fct_daily_bank_account_balance.c.merchant_guid,
+            fct_daily_bank_account_balance.c.account_guid,
+        )
+        .cte("last_transaction_by_account")
+    )
+
+    statement = (
+        select(
+            columns=[
+                last_transaction_by_account_query.c.merchant_guid,
+                func.object_agg(
+                    last_transaction_by_account_query.c.account_guid,
+                    sqlalchemy.cast(
+                        last_transaction_by_account_query.c.last_transaction, VARIANT
+                    ),
+                ).label("last_transaction_dates"),
+            ]
+        )
+        .where(last_transaction_by_account_query.c.merchant_guid == merchant_guid)
+        .where(last_transaction_by_account_query.c.account_guid.in_(account_guids))
+        .group_by(last_transaction_by_account_query.c.merchant_guid)
+        .select_from(last_transaction_by_account_query)
+    )
+
+    return statement
+
+
+def debits_credits_query(
+    snowflake_analytics_connection: str,
+    analytics_schema: str,
+    merchant_guid: str,
+    account_guids: List[str],
+) -> Select:
+    metadata = MetaData()
+    production_engine = SnowflakeHook(
+        snowflake_analytics_connection
+    ).get_sqlalchemy_engine()
+
+    fct_daily_bank_account_balance = Table(
+        "fct_daily_bank_account_balance",
+        metadata,
+        autoload=True,
+        autoload_with=production_engine,
+        schema=analytics_schema,
+    )
+
+    summed_accounts_query = (
+        select(
+            columns=[
+                fct_daily_bank_account_balance.c.merchant_guid,
+                func.array_construct(account_guids).label("account_guids"),
+                fct_daily_bank_account_balance.c.date,
+                func.sum(fct_daily_bank_account_balance.c.credits).label("sum_credits"),
+                func.sum(fct_daily_bank_account_balance.c.debits).label("sum_debits"),
+                func.sum(fct_daily_bank_account_balance.c.opening_balance).label(
+                    "sum_opening_balance"
+                ),
+            ],
+            from_obj=fct_daily_bank_account_balance,
+        )
+        .where(fct_daily_bank_account_balance.c.merchant_guid == merchant_guid)
+        .where(fct_daily_bank_account_balance.c.account_guid.in_(account_guids))
+        .group_by(
+            fct_daily_bank_account_balance.c.merchant_guid,
+            fct_daily_bank_account_balance.c.date,
+        )
+        .order_by(fct_daily_bank_account_balance.c.date.desc())
+        .cte("summed_accounts")
+    )
+
+    object_debits_credits_query = (
+        select(
+            columns=[
+                summed_accounts_query.c.merchant_guid,
+                summed_accounts_query.c.account_guids,
+                summed_accounts_query.c.date,
+                func.object_construct(
+                    text("'debits'"),
+                    summed_accounts_query.c.sum_debits,
+                    text("'credits'"),
+                    summed_accounts_query.c.sum_credits,
+                    text("'balance'"),
+                    summed_accounts_query.c.sum_opening_balance,
+                ).label("object"),
+            ],
+        )
+        .select_from(summed_accounts_query)
+        .order_by(summed_accounts_query.c.date.desc())
+        .cte("object_debits_credits")
+    )
+
+    statement = (
+        select(
+            columns=[
+                object_debits_credits_query.c.merchant_guid,
+                object_debits_credits_query.c.account_guids,
+                func.object_agg(
+                    object_debits_credits_query.c.date,
+                    object_debits_credits_query.c.object,
+                ).label("daily_cash_flow"),
+            ]
+        )
+        .group_by(
+            object_debits_credits_query.c.merchant_guid,
+            object_debits_credits_query.c.account_guids,
+        )
+        .select_from(object_debits_credits_query)
+    )
+
+    return statement
+
+
+def generate_multi_projections(
+    snowflake_zetatango_connection: str,
+    snowflake_analytics_connection: str,
+    task_instance_key_str: str,
+    ts_nodash: str,
+    analytics_schema: str,
+    zetatango_schema: str,
+    dag_run: DagRun,
+    **kwargs: Any,
+) -> None:
+    metadata = MetaData()
+    production_engine = SnowflakeHook(
+        snowflake_analytics_connection
+    ).get_sqlalchemy_engine()
+    zetatango_engine = SnowflakeHook(
+        snowflake_zetatango_connection
+    ).get_sqlalchemy_engine()
+
+    if "merchant_guid" not in dag_run.conf or "account_ids" not in dag_run.conf:
+        logging.error(
+            f"❌ Unable to execute multi projection without merchant_guid or account_ids"
+        )
+
+        return
+
+    merchant_guid = dag_run.conf["merchant_guid"]
+    account_guids = dag_run.conf["account_ids"]
+
+    # Make sure account guids are sorted
+    account_guids.sort()
+
+    projection_id: str = sha256(
+        f"{task_instance_key_str}_{ts_nodash}".encode("utf-8")
+    ).hexdigest()
+
+    with production_engine.begin() as tx:
+        debit_credits_statement = debits_credits_query(
+            snowflake_analytics_connection,
+            analytics_schema,
+            merchant_guid,
+            account_guids,
+        )
+
+        row = tx.execute(debit_credits_statement).first()
+        cash_flow_df = format_cash_flow_into_df(row)
+
+        try:
+            do_multi_account_projection(
+                merchant_guid,
+                account_guids,
+                cash_flow_df,
+                projection_id,
+                snowflake_zetatango_connection,
+                snowflake_analytics_connection,
+                analytics_schema,
+                zetatango_schema,
+            )
+        except:
+            e = sys.exc_info()[0]
+            logging.error(f"Error: {e}")
+
+
+def get_account_last_transaction_date(
+    snowflake_analytics_connection: str,
+    analytics_schema: str,
+    merchant_guid: str,
+    account_guids: List[str],
+) -> Dict[str, str]:
+    metadata = MetaData()
+    production_engine = SnowflakeHook(
+        snowflake_analytics_connection
+    ).get_sqlalchemy_engine()
+
+    with production_engine.begin() as tx:
+        last_transaction_statement = last_transaction_date_query(
+            snowflake_analytics_connection,
+            analytics_schema,
+            merchant_guid,
+            account_guids,
+        )
+
+        row = tx.execute(last_transaction_statement).first()
+
+        last_transactions = json.loads(row["last_transaction_dates"])
+
+        return last_transactions
+
+
+def do_multi_account_projection(
+    merchant_guid: str,
+    account_guids: List[str],
+    cash_flow_df: pd.DataFrame,
+    projection_id: str,
+    snowflake_zetatango_connection: str,
+    snowflake_analytics_connection: str,
+    analytics_schema: str,
+    zetatango_schema: str,
+) -> None:
+    auto_arima_parameters = AutoArimaParameters()
+    arima_projection_parameters = ArimaProjectionParameters()
+    cash_flow_projection_parameters = CashFlowProjectionParameters()
+
+    logging.info(
+        f"Generating projections for {account_guids} for merchant {merchant_guid}"
+    )
+
+    transactions_info = get_account_last_transaction_date(
+        snowflake_analytics_connection,
+        analytics_schema,
+        merchant_guid,
+        account_guids,
+    )
+
+    logging.info(f"Last transactions: {transactions_info}")
+
+    details = {
+        "id": projection_id,
+        "version": cash_flow_projection_parameters.version,
+        "auto_arima_params": attr.asdict(auto_arima_parameters),
+        "arima_projection_params": attr.asdict(arima_projection_parameters),
+        "last_transaction_dates": dict(
+            sorted(transactions_info.items(), key=lambda item: item[0])
+        ),
+    }
+
+    parameters_to_hash: Dict[str, Dict[str, Any]] = {
+        "auto_arima_params": cast(Dict[str, Any], details["auto_arima_params"]),
+        "arima_projection_params": cast(
+            Dict[str, Any], details["arima_projection_params"]
+        ),
+        "cash_flow_projection_parameters": cast(Dict[str, Any], details["version"]),
+        "last_transaction_dates": cast(
+            Dict[str, str], details["last_transaction_dates"]
+        ),
+    }
+    parameters_to_hash["auto_arima_params"].pop("random_state", None)
+
+    if skip_multi_account_projection(
+        merchant_guid,
+        account_guids,
+        snowflake_zetatango_connection,
+        zetatango_schema,
+        parameters_to_hash,
+    ):
+        return
+
+    if apply_pre_projection_guardrails(cash_flow_df, arima_projection_parameters):
+        logging.warning(
+            f"❌ Pre projection guardrail for {merchant_guid} - {account_guids} applied"
+        )
+
+        store_multi_account_projection(
+            merchant_guid,
+            account_guids,
+            cash_flow_df,
+            parameters_to_hash,
+            details,
+            snowflake_zetatango_connection,
+            zetatango_schema,
+        )
+
+        return
+
+    projection_df = calculate_projection(
+        cash_flow_df,
+        auto_arima_parameters,
+        arima_projection_parameters,
+    )
+
+    if apply_post_projection_guardrail_closing_balance(cash_flow_df, projection_df):
+        logging.warning(
+            f"❌ Post projection guardrail on closing balance for {merchant_guid} - {account_guids} applied"
+        )
+
+        store_multi_account_projection(
+            merchant_guid,
+            account_guids,
+            cash_flow_df,
+            parameters_to_hash,
+            details,
+            snowflake_zetatango_connection,
+            zetatango_schema,
+        )
+
+        return
+
+    if guardrail_linear_regression(cash_flow_df, projection_df):
+        logging.warning(
+            f"❌ Post projection guardrail on linear regression for {merchant_guid} - {account_guids} applied"
+        )
+
+        store_multi_account_projection(
+            merchant_guid,
+            account_guids,
+            cash_flow_df,
+            parameters_to_hash,
+            details,
+            snowflake_zetatango_connection,
+            zetatango_schema,
+        )
+
+        return
+
+    projection_df.index = pd.to_datetime(projection_df.index).astype(str)
+    details["data"] = projection_df.to_dict("index")
+
+    store_multi_account_projection(
+        merchant_guid,
+        account_guids,
+        cash_flow_df,
+        parameters_to_hash,
+        details,
+        snowflake_zetatango_connection,
+        zetatango_schema,
+    )
+
+
+def format_cash_flow_into_df(cash_flow_result: RowProxy) -> pd.DataFrame:
+    df = pd.DataFrame.from_dict(
+        json.loads(cash_flow_result["daily_cash_flow"]), orient="index"
+    )
+
+    df.index = pd.to_datetime(df.index)
+
+    full_index = pd.date_range(df.index.min(), df.index.max())
+    df = df.reindex(full_index)
+
+    df.index.rename(inplace=True, name="DateTime")
+    df.sort_index(inplace=True)
+
+    for column in ["credits", "debits"]:
+        df[column].fillna(0, inplace=True)
+
+    df["balance"].ffill(inplace=True)
+
+    return df
+
+
 def generate_projections(
     snowflake_zetatango_connection: str,
     snowflake_analytics_connection: str,
@@ -572,6 +1068,16 @@ def create_dag() -> DAG:
                 "analytics_schema": "DBT_ARIO",
                 "zetatango_schema": "CORE_PRODUCTION",
             },
+        ) >> PythonOperator(
+            task_id="generate_multi_projections",
+            python_callable=generate_multi_projections,
+            provide_context=True,
+            op_kwargs={
+                "snowflake_zetatango_connection": "snowflake_zetatango_production",
+                "snowflake_analytics_connection": "airflow_production",
+                "analytics_schema": "DBT_ARIO",
+                "zetatango_schema": "CORE_PRODUCTION",
+            },
         ) >> DbtOperator(
             task_id="dbt_run_generate_projections",
             execution_timeout=timedelta(hours=1),
@@ -614,6 +1120,22 @@ if __name__ == "__main__":
     #         "task_ts",
     #         "DBT_ARIO",
     #         "CORE_STAGING",
+    #     )
+
+    #     generate_multi_projections(
+    #         "snowflake_zetatango_production",
+    #         "snowflake_analytics_connection",
+    #         "task_id",
+    #         "task_ts",
+    #         "DBT_ARIO",
+    #         "CORE_STAGING",
+    #         "m_XkunNiHLGkcWcmw6",
+    #         [
+    #             "088c1bed-4dfd-4735-a4df-47e26e8e582f",
+    #             "838e6f3c-78fb-4a2c-bcdd-3b2634d4b820",
+    #             "a435f82c-ee7e-4413-a308-712b5f291ae9",
+    #         ],
+    #         None,
     #     )
 else:
     globals()["cash_flow_projection"] = create_dag()
