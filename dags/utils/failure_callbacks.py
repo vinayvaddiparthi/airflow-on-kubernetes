921d605781c205f6bf9d26c9367b0c0b4c535ece
Jim Kim <jim.kim@thinkingcapital.ca>
2021-06-09 14:27:11 -0400

    updated slack_dag function to return more descriptive errors

--- dags/utils/failure_callbacks.py
+++ dags/utils/failure_callbacks.py
@@ -19,7 +19,11 @@ def slack_dag(conn_id: str) -> Callable:
         return SlackWebhookOperator(
             task_id="slack_fail",
             http_conn_id=conn_id,
-            message=f'DagRun failed: {context["dag_run"]}',
+            message=f':red_circle: Task failed. {context["task_instance"]}\n'
+                    f'*Task*: {context["task_instance"].task_id}\n'
+                    f'*Dag*: {context["task_instance"].dag_id}\n'
+                    f'*Execution Time*: {context["execution_date"]}\n'
+                    f'*Log Url*: {context["task_instance"].log_url}',
         ).execute(context=context)
 
     return func
