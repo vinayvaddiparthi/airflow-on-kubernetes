dca165897781571d051492285f849a6e21a84fe3
Jim Kim <jim.kim@thinkingcapital.ca>
2021-08-05 21:40:30 -0400

    add default args

--- dags/equifax_consumer_outbound_dag.py
+++ dags/equifax_consumer_outbound_dag.py
@@ -5,6 +5,17 @@ This workflow sends the Equifax consumer request file (i.e. eligible applicant i
 Equifax on a monthly basis for recertification purposes.
 """
 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['enterprisedata@thinkingcapital.ca'],
+    'email_on_failure': False,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'on_failure_callback': slack_dag('slack_data_alerts'),
+}
+
 dag = DAG(
     "equifax_consumer_outbound",
     default_args=default_args,
