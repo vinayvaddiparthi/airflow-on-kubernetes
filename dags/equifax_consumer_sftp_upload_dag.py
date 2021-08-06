a3e6bad6d76d9f63843e8adcba712c32ec8911be
Jim Kim <jim.kim@thinkingcapital.ca>
2021-08-05 21:27:30 -0400

    add DAG documentation

---
+++ dags/equifax_consumer_sftp_upload_dag.py
@@ -0,0 +1,8 @@
+"""
+# Equifax Consumer SFTP Upload DAG
+
+This workflow sends the Equifax consumer request file (i.e. eligible applicant information) to
+Equifax on a monthly basis for recertification purposes.
+"""
+
+dag.doc_md = __doc__
