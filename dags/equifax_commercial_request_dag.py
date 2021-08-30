63aebaa5e95daeb4d483434bc6f7bc8fdf21f0e8
Jim Kim <jim.kim@thinkingcapital.ca>
2021-08-30 15:29:30 -0400

    generate commercial request file every odd month only

--- dags/equifax_commercial_request_dag.py
+++ dags/equifax_commercial_request_dag.py
@@ -24,14 +24,16 @@ default_args = {
     "start_date": datetime(2020, 1, 1, 00, 00, 00),
     "concurrency": 1,
     "retries": 3,
+    "on_failure_callback": slack_dag("slack_data_alerts"),
+    "catchup": False,
+    "tags": ["equifax"],
+    "description": "A workflow to generate the commercial batch request file to send to Equifax.",
 }
 
 dag = DAG(
     dag_id="equifax_commercial_request",
-    catchup=False,
     default_args=default_args,
-    schedule_interval="0 0 1 * *",  # Run once a month at midnight of the first day of the month
-    on_failure_callback=slack_dag("slack_data_alerts"),
+    schedule_interval="0 0 1 1-11/2 *",
 )
 
 snowflake_connection = "airflow_production"
