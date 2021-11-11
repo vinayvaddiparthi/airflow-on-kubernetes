09425f8f10798345c1532d237898577ea52b10bd
Judy Xiao <juntong.xiao@thinkingcapital.ca>
2021-11-10 22:05:38 -0500

    use historical dag to import email event

--- dags/historical_google_analytics_import.py
+++ dags/historical_google_analytics_import.py
@@ -4,7 +4,7 @@ import tempfile
 from typing import Any, Dict
 from pathlib import Path
 import pendulum
-from datetime import timedelta, datetime
+from datetime import timedelta
 from airflow import DAG
 from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
 from airflow.hooks.base_hook import BaseHook
@@ -12,7 +12,6 @@ from airflow.operators.python_operator import PythonOperator
 
 from utils import random_identifier
 from utils.failure_callbacks import slack_dag
-from data.google_analytics import reports
 from google_analytics_import import (
     initialize_analytics_reporting,
     get_report,
@@ -47,7 +46,6 @@ def transform_raw_json(raw: Dict, ds: str) -> Any:
 with DAG(
     dag_id="historical_google_analytics_import",
     max_active_runs=1,
-    concurrency=1,
     schedule_interval=None,
     default_args={"retries": 2, "retry_delay": timedelta(minutes=5)},
     catchup=True,
@@ -56,23 +54,12 @@ with DAG(
     ),
     on_failure_callback=slack_dag("slack_data_alerts"),
 ) as dag:
-    end_date = "2021-10-19"
-    DAYS = 5
+    END_DATE = "2021-10-29" #-28
+    START_DATE = "2021-07-05"
 
-    def build_deduplicate_query(dest_db: str, dest_schema: str, table: str) -> str:
-        query = f"merge into {dest_db}.{dest_schema}.{table} using {dest_db}.{dest_schema}.historical_{table}_stage on "  # nosec
-        for key in reports[table]["primary_keys"]:  # type: ignore
-            query += f"{dest_db}.{dest_schema}.{table}.{key} = {dest_db}.{dest_schema}.historical_{table}_stage.{key} and "
-        query = query[:-4]
-        query += "when matched then delete"
-        return query
-
-    def process(table: str, conn: str, end_date: str, **context: Any) -> None:
+    def process(table: str, conn: str, **context: Any) -> None:
         ds = context["ds"]
-        start_date = (
-            datetime.strptime(end_date, "%Y-%m-%d") - timedelta(days=(DAYS - 1))
-        ).strftime("%Y-%m-%d")
-        logging.info(f"Date Range: {start_date} - {end_date}")
+        logging.info(f"Date Range: {START_DATE} - {END_DATE}")
         analytics = initialize_analytics_reporting()
         google_analytics_hook = BaseHook.get_connection("google_analytics_snowflake")
         dest_db = google_analytics_hook.extra_dejson.get("dest_db")
@@ -92,7 +79,7 @@ with DAG(
             page_token: Any = "0"
             while page_token:
                 response = get_report(
-                    analytics, table, start_date, end_date, page_token
+                    analytics, table, START_DATE, END_DATE, page_token
                 )
                 if response:
                     res_json = transform_raw_json(response, ds)
@@ -114,32 +101,25 @@ with DAG(
                     page_token = None
 
             tx.execute(
-                f"create or replace table {dest_db}.{dest_schema}.historical_{table}_stage as "  # nosec
+                f"create or replace table {dest_db}.{dest_schema}.{table} as "  # nosec
                 f"select $1 as fields from @{dest_schema}.{stage_guid}"  # nosec
             )
-            if "primary_keys" in reports[table]:  # type: ignore
-                tx.execute(build_deduplicate_query(dest_db, dest_schema, table))
             tx.execute(
-                f"insert into {dest_db}.{dest_schema}.{table} "  # nosec
-                f"select * from {dest_db}.{dest_schema}.historical_{table}_stage"  # nosec
+                f"GRANT SELECT ON TABLE {dest_db}.{dest_schema}.{table} TO ROLE DBT_DEVELOPMENT"
             )
-            tx.execute(f"drop table {dest_db}.{dest_schema}.historical_{table}_stage")
-
+            tx.execute(
+                f"GRANT SELECT ON TABLE {dest_db}.{dest_schema}.{table} TO ROLE DBT_PRODUCTION"
+            )
             logging.info(
                 f"✔️ Successfully loaded historical {table} for {start_date} - {end_date} on {ds}"
             )
 
-    while end_date >= "2021-07-05":
-        dag << PythonOperator(
-            task_id=f"import_acquisition_funnel_{end_date}",
-            python_callable=process,
-            op_kwargs={
-                "conn": "snowflake_production",
-                "table": "acquisition_funnel",
-                "end_date": end_date,
-            },
-            provide_context=True,
-        )
-        end_date = (
-            datetime.strptime(end_date, "%Y-%m-%d") - timedelta(days=DAYS)
-        ).strftime("%Y-%m-%d")
+    dag << PythonOperator(
+        task_id=f"import_server_cx_email_event",
+        python_callable=process,
+        op_kwargs={
+            "conn": "snowflake_production",
+            "table": "server_cx_email",
+        },
+        provide_context=True,
+    )
