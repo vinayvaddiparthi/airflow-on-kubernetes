67260dd106ed7b60f6fd12c01cd2ee2f0fd48fa8
Jim Kim <jim.kim@thinkingcapital.ca>
2021-12-17 18:07:16 -0500

    create a dag to test equifax sftp server connection

---
+++ dags/equifax_sftp_server_connection_test.py
@@ -0,0 +1,31 @@
+import pendulum
+import logging
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.sftp.hooks.sftp import SFTPHook
+
+
+def check_sftp_connection(ftp_conn_id: str):
+    hook = SFTPHook(ftp_conn_id=ftp_conn_id)
+    inbox_files = hook.list_directory(path="inbox/")
+    outbox_files = hook.list_directory(path="outbox/")
+    logging.info(f"inbox_files: {inbox_files}")
+    logging.info(f"outbox_files: {outbox_files}")
+
+
+with DAG(
+    dag_id="equifax_sftp_server_connection_test",
+    default_args={"owner": "airflow"},
+    start_date=pendulum.datetime(
+        2021, 12, 17, tzinfo=pendulum.timezone("America/Toronto")
+    ),
+    schedule_interval=None,
+    description="test connection to equifax sftp server for batch bureau pull",
+    tag="equifax",
+) as dag:
+
+    check_sftp_connection = PythonOperator(
+        task_id="check_sftp_connection",
+        python_callable=check_sftp_connection,
+        op_kwargs={"ftp_conn_id": "equifax_sftp"},
+    )
