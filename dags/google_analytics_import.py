c8ed8fb476ac0314e65e475bfcc2c6751a709ee2
Salahddin-Warid <salahddin.warid@thinkingcapital.ca>
2022-04-04 13:49:52 -0400

    Black reformat

--- dags/google_analytics_import.py
+++ dags/google_analytics_import.py
@@ -119,8 +119,8 @@ def transform_raw_json(raw: Dict, ds: str, import_ts: str) -> Any:
 with DAG(
     dag_id="google_analytics_import",
     max_active_runs=1,
-        schedule_interval="0 */2 * * *",
-        default_args={
+    schedule_interval="0 */2 * * *",
+    default_args={
         "retries": 2,
         "retry_delay": timedelta(minutes=5),
         "on_failure_callback": slack_task("slack_data_alerts"),
