efadd28ed75fc5a761489c4c29f9a1bff2066b36
Salahddin-Warid <salahddin.warid@thinkingcapital.ca>
2022-04-14 18:06:11 -0400

    Trying without insert statement

--- dags/zetatango_import.py
+++ dags/zetatango_import.py
@@ -218,11 +218,6 @@ def stage_table_in_snowflake(
                 f"select $1 as fields from @{destination_schema}.{stage_guid}"  # nosec
             ).fetchall()
 
-            tx.execute(
-                f"insert into {destination_schema}.{table} "
-                f"select $1 as fields from @{destination_schema}.{stage_guid}_part_2"
-            ).fetchall()
-
         else:
             tx.execute(
                 f"create or replace transient table {destination_schema}.{table} as "  # nosec
