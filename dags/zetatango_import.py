d99c886c9cd2582af9724d44adc8029c4b614205
Salahddin-Warid <salahddin.warid@thinkingcapital.ca>
2022-04-14 23:56:02 -0400

    Properly creating the temporary files

--- dags/zetatango_import.py
+++ dags/zetatango_import.py
@@ -144,14 +144,14 @@ def stage_table_in_snowflake(
         if table == "lending_adjudications":
             csv_file_dir_split_1 = tempfile.TemporaryDirectory()
             csv_file_dir_split_2 = tempfile.TemporaryDirectory()
-            pq_file_dir = tempfile.TemporaryDirectory()
+            pq_file_dir_2 = tempfile.TemporaryDirectory()
             csv_filepath_split_1 = Path(csv_file_dir_split_1.name, table).with_suffix(
                 ".csv"
             )
             csv_filepath_split_2 = Path(csv_file_dir_split_2.name, table).with_suffix(
                 ".csv"
             )
-            pq_filepath_2 = Path(pq_file_dir.name, table).with_suffix(".pq")
+            pq_filepath_2 = Path(pq_file_dir_2.name, table).with_suffix(".pq")
             tx.execute(
                 f"create or replace temporary stage {destination_schema}.{stage_guid_part_2} "
                 f"file_format=(type=parquet)"
