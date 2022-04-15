2325ae39d375c8dc5fe07e39f41a5debd9f155c3
Ying Li <ying.li@zetatango.com>
2022-04-15 02:35:55 +0000

    Update dags/zetatango_import.py

--- dags/zetatango_import.py
+++ dags/zetatango_import.py
@@ -143,10 +143,10 @@ def stage_table_in_snowflake(
 
         if table == "lending_adjudications":
             csv_filepath_split_1 = Path(
-                tempfile.TemporaryDirectory().name, table
+                tempdir, f"{table}_1"
             ).with_suffix(".csv")
             csv_filepath_split_2 = Path(
-                tempfile.TemporaryDirectory().name, table
+                tempdir, f"{table}_2"
             ).with_suffix(".csv")
             pq_filepath_2 = Path(tempfile.TemporaryDirectory().name, table).with_suffix(
                 ".pq"
