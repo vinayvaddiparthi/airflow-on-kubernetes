ddb08e61800424740b86324caf166c51e062f71c
Vinay Vaddiparthi <vinay.vaddiparthi@thinkingcapital.ca>
2022-05-27 09:50:27 -0400

    Fixed linting errors

--- dags/insights_benchmarks.py
+++ dags/insights_benchmarks.py
@@ -94,7 +94,7 @@ def _classify_transactions(
             df_lookup["cash_flow_lookup_version_id"]
             == df_lookup["cash_flow_lookup_version_id"].max()
         ]
-        
+
         df_lookup.sort_values("position", ascending=True, inplace=True)
 
         df_precise_entries = df_lookup[df_lookup["has_match"]]
