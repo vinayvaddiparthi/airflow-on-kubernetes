c30405f2041dc57e39330acad4d05861ba0f2d12
Smridhisood <smridhi.sood@driven.ca>
2022-07-19 15:59:25 -0400

    Updated the code to use context variable ts instead of ds

--- dags/google_analytics_import.py
+++ dags/google_analytics_import.py
@@ -17,7 +17,6 @@ from airflow.operators.python_operator import PythonOperator
 from googleapiclient.discovery import build as AnalyticsBuild
 from oauth2client.service_account import ServiceAccountCredentials
 from dateutil import tz
-import pytz
 
 from utils import random_identifier
 from utils.failure_callbacks import slack_task
@@ -152,12 +151,16 @@ with DAG(
 
     def process(table: str, conn: str, **context: Any) -> None:
         ds = context["ds"]
-        ds_utc = datetime.strptime(ds, "%Y-%m-%d")
-        ds_utc = ds_utc.replace(tzinfo=pytz.UTC)
-        tz_est = tz.gettz("America/Toronto")
-        ds_est = ds_utc.astimezone(tz_est).date()
+        ts = context["ts"]
+        ts_date = datetime.fromisoformat(ts)
+        ts_est = (
+            ts_date.astimezone(tz.gettz("America/Toronto")).date().strftime("%Y-%m-%d")
+        )
         logging.info(f"Date Range: {ds}")
-        logging.info(f"Date Range (EST): {ds_est}")
+        logging.info(f"Date Range (EST): {ts_est}")
+        ds = ts_est
+        logging.info(f"Date Range: {ds}")
+        logging.info(f"Date Range (EST): {ts_est}")
         utc_time_now = get_utc_timestamp()
         analytics = initialize_analytics_reporting()
         google_analytics_hook = BaseHook.get_connection("google_analytics_snowflake")
